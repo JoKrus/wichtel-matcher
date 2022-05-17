345c12e9440284a4ef1035dcdd8a44ca3b0bac32
JoKrus <JoKrus@web.de>
2022-05-17 13:03:22 +0200

    mail sending supported now

---
+++ src/main/java/net/jcom/rap/wichteln/MailerConfigWrapper.java
@@ -0,0 +1,4 @@
+package net.jcom.rap.wichteln;
+
+public record MailerConfigWrapper(String host, int port, String mail, String password, String name) {
+}
--- src/main/java/net/jcom/rap/wichteln/Main.java
+++ src/main/java/net/jcom/rap/wichteln/Main.java
@@ -1,16 +1,38 @@
 package net.jcom.rap.wichteln;
 
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
 import java.nio.charset.Charset;
 
 public class Main {
     public static void main(String[] args) {
-        var participantsString = ResourceLoader.readResourceAsList("participantsTEMPLATE.txt",
-                Charset.defaultCharset());
+        var participantsString = ResourceLoader.readResourceAsList("participantsTEST1.txt", Charset.defaultCharset());
         var participants = participantsString.stream().map(s -> {
             var split = s.split(";");
             return new Participant(split[0], split[1]);
         }).toList();
 
-        participants.forEach(System.out::println);
+        var mailerStrings = ResourceLoader.readResourceAsList("mailerconfig.txt", Charset.defaultCharset());
+
+        var mailerConfigWrapper = new MailerConfigWrapper(mailerStrings.get(0), Integer.parseInt(mailerStrings.get(1)),
+                mailerStrings.get(2), mailerStrings.get(3), mailerStrings.get(4));
+
+        var mailer = MailerBuilder.withSMTPServer(mailerConfigWrapper.host(), mailerConfigWrapper.port(),
+                        mailerConfigWrapper.mail(),
+                        mailerConfigWrapper.password())
+                .withTransportStrategy(TransportStrategy.SMTP_TLS).buildMailer();
+
+        for (var part : participants) {
+            var mail = EmailBuilder.startingBlank().to(part.name(), part.mailAddress())
+                    .withSubject("WichtelTestMail").withPlainText("Einfach Parsa man").from(mailerConfigWrapper.name(),
+                            mailerConfigWrapper.mail())
+                    .buildEmail();
+
+            mailer.sendMail(mail);
+
+            System.out.printf("Mail sent to %s%n", part.name());
+        }
     }
 }
